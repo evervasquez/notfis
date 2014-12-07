5812317160fdeb82c941279704cd76942bef5c4e
brapastor <brapastor@gmail.com>
2014-12-07 11:40:03 -0500

    Arreglo de Url

--- app/controllers/NoticiasController.php
+++ app/controllers/NoticiasController.php
@@ -34,7 +34,7 @@ class NoticiasController extends \BaseController
         //url imagen
         $urlimagen = $file->getClientOriginalName();
 
-        $datos['imagen_url'] =  "http://".$_SERVER['SERVER_NAME']."/assets/img/notiImages/".$urlimagen;;
+        $datos['imagen_url'] =  $urlimagen;
 
         $noticia = $this->noticiaRepo->nuevoNoticiaGCM($datos);
 
